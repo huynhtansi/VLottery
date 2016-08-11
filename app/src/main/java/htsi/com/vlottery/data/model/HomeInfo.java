88e47c1ae5d4ce25a51aec05d3a824eb8e401368
Huỳnh Tấn Sĩ <si.huynh@dinosys.vn>
2016-08-11 13:42:35 +0700

    Ready release for beta2

--- app/src/main/java/htsi/com/vlottery/data/model/HomeInfo.java
+++ app/src/main/java/htsi/com/vlottery/data/model/HomeInfo.java
@@ -2,21 +2,18 @@ package htsi.com.vlottery.data.model;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
-import java.util.Map;
-
 /**
  * Created by Huỳnh Phúc on 8/6/2016.
  */
 @IgnoreExtraProperties
 public class HomeInfo {
 
-    String jackpot;
-    String lastResult;
-    String lastTimeResult;
-    String status;
-    String nextTimeResult;
-    int rollingBall;
-    Map<String, Integer> ballValues;
+    public String jackpot;
+    public String lastResult;
+    public String lastTimeResult;
+    public String status;
+    public String nextTimeResult;
+    public int rollingBall;
 
     public HomeInfo() {}
 
@@ -24,47 +21,47 @@ public class HomeInfo {
         return jackpot;
     }
 
+    public void setJackpot(String pJackpot) {
+        jackpot = pJackpot;
+    }
+
     public String getLastResult() {
         return lastResult;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public String getNextTimeResult() {
-        return nextTimeResult;
-    }
-
-    public int getRollingBall() {
-        return rollingBall;
+    public void setLastResult(String pLastResult) {
+        lastResult = pLastResult;
     }
 
     public String getLastTimeResult() {
         return lastTimeResult;
     }
 
-    public Map<String, Integer> getBallValues() {
-        return ballValues;
+    public void setLastTimeResult(String pLastTimeResult) {
+        lastTimeResult = pLastTimeResult;
     }
 
-    public void setJackpot(String jackpot) {
-        this.jackpot = jackpot;
+    public String getStatus() {
+        return status;
     }
 
-    public void setLastResult(String lastResult) {
-        this.lastResult = lastResult;
+    public void setStatus(String pStatus) {
+        status = pStatus;
     }
 
-    public void setLastTimeResult(String lastTimeResult) {
-        this.lastTimeResult = lastTimeResult;
+    public String getNextTimeResult() {
+        return nextTimeResult;
     }
 
-    public void setNextTimeResult(String nextTimeResult) {
-        this.nextTimeResult = nextTimeResult;
+    public void setNextTimeResult(String pNextTimeResult) {
+        nextTimeResult = pNextTimeResult;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public int getRollingBall() {
+        return rollingBall;
+    }
+
+    public void setRollingBall(int pRollingBall) {
+        rollingBall = pRollingBall;
     }
 }
